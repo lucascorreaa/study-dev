67daa7b3b43cec6f505ba9884f641683b7036672
lucascorreaa <lucas.lca14@gmail.com>
2024-12-10 17:09:13 -0300

    added javascript fundamentals

---
+++ src/js/fundamentals.js
@@ -0,0 +1,26 @@
+// variables 
+let variavel =  'sou alteravel'
+const constante = 'não é redeclaravel'
+
+variavel = 1
+// proibido reassinar ma constante
+// constante = 2
+
+// console
+console.log(variavel)
+console.error(constante)
+console.info('sou um informação')
+console.warn('var é depreciado')
+// console.table(fruits)
+
+// tipos de dados
+const lastName = 'correa' // string
+let age = 25 //number
+const person = {
+    name: 'joao',
+    age: 16,
+    gender: 'male'
+} // objeto
+const fruits = ['banana', 'uva'] //array
+
+
